ce6c0525fc72cf958cd1f22fcd253bc61400707a
258333601@163.com <258333601@163.com>
2020-12-16 13:02:53 +0400

    调整ios 证书

--- iosSign/startSign.sh
+++ iosSign/startSign.sh
@@ -3,4 +3,5 @@ srcIpa=$1
 target=$2
 shellDir=$(cd `dirname $0`; pwd)
 echo $shellDir
-sh $shellDir/reSign.sh $srcIpa  'iPhone Distribution: Nik Suriati Akma Binti Muhammad Sabri' "$shellDir/779.mobileprovision"  $target
+#sh $shellDir/reSign.sh $srcIpa  'iPhone Distribution: Nik Suriati Akma Binti Muhammad Sabri' "$shellDir/779.mobileprovision"  $target
+sh $shellDir/reSign.sh $srcIpa  'Apple Distribution: Guilin Goodsound Network Technology Co., Ltd.' "$shellDir/valley.mobileprovision"  $target
